f852560898ff709f7d4621ea5522f8834fb3e48f
gadiz <gadizad@gmail.com>
2013-10-15 21:20:35 +0300

    Adding sensor plate

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -4,19 +4,28 @@
 
 
 #include <Servo.h> 
- 
+#include <Narcoleptic.h>
+//#include "LowPower.h"
+/* 
+A library that allows you to send the microcontroller in standby
+in times of inactivity (delay), saving a lot of energy.
+You can not possibly use (in this case must be replaced
+the last statement Narcoleptic.delay (15) with delay (15).
+*/
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
  
 int pos = 90;    // variable to store the servo position 
- 
+
+#define SERVO_D 8
+
 void setup() 
 { 
   Serial.begin(9600);
-  myservo.attach(8);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(SERVO_D);  // attaches the servo on pin 9 to the servo object 
 } 
  
- 
+int countPrint = 0; 
 void loop() 
 { 
  /* for(pos = 0; pos < 180; pos += 10)  // goes from 0 degrees to 180 degrees 
@@ -30,27 +39,31 @@ void loop()
     delay(150);                       // waits 15ms for the servo to reach the position 
   }
  */
-  float a = analogRead(A0);
-  float b = analogRead(A1);
-  float delta = fabs(a-b);
-  if (delta > 3){
-    int boost = 0;
-    if (delta > 10)
-      boost = 10;
-    if (a > b){
-      pos += (1 + boost);
-      if (pos > 150)
-        pos = 150;
+  float left = analogRead(A0);
+  float right = analogRead(A1);
+  float delta = fabs(left-right);
+  if (delta > 2){
+//    myservo.detach();
+ // else{
+  //  myservo.attach(SERVO_D); 
+    if (left > right){
+      pos += min(20 , delta/2);
+      if (pos > 160)
+        pos = 160;
       else 
         myservo.write(pos);
     }else{
-      pos -= (1 + boost);
-      if (pos < 30)
-        pos = 30;
+      pos -= min(20  , delta/2);
+      if (pos < 10)
+        pos = 10;
        else
          myservo.write(pos);  
     }
   }
+  ///Narcoleptic.delay (100);
+   // Sleep for 8 s with ADC module and BOD module off
+//    LowPower.powerDown(SLEEP_8S, ADC_CONTROL_OFF, BOD_OFF);
+//    See more at: http://www.rocketscream.com/blog/2011/07/04/lightweight-low-power-arduino-library/#sthash.noeikouj.dpuf
   delay (100);
   
 
@@ -58,7 +71,10 @@ void loop()
      //delay(2000);
      //myservo.write(60);
      //delay(2000);
-     Serial.print(a);
-     Serial.print(b);
-     Serial.print("\n\r");
+     countPrint++;
+     if (countPrint%100 == 0){
+       Serial.print(left);
+       Serial.print(right);
+       Serial.print("\n\r");
+     }
 } 
